c32edda00ce5a5c8445ac4e641ad377d9c893ed6
enisfr <enisfurkane@gmail.com>
2024-12-11 20:50:06 +0100

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package com.enisfr
+
+import com.enisfr.days.Day1
+
+fun main() {
+    Day1().part2()
+}
---
+++ src/main/kotlin/days/Day1.kt
@@ -0,0 +1,44 @@
+package com.enisfr.days
+
+import java.io.File
+import kotlin.math.abs
+
+class Day1(private val filePath: String = "src/main/resources/inputs/1/input.txt") {
+    private val leftPoints = mutableListOf<Int>()
+    private val rightPoints = mutableListOf<Int>()
+
+    private fun parseFile() {
+        leftPoints.clear()
+        rightPoints.clear()
+
+        File(filePath).useLines { lines ->
+            lines.forEach { line ->
+                val (left, right) = line.trim().split("\\s+".toRegex()).map { it.toInt() }
+                leftPoints.add(left)
+                rightPoints.add(right)
+            }
+        }
+    }
+
+    fun part1() {
+        parseFile()
+        leftPoints.sort()
+        rightPoints.sort()
+
+        val distance = leftPoints.indices.sumOf { i ->
+            abs(rightPoints[i] - leftPoints[i])
+        }
+
+        println("Part 1: $distance")
+    }
+
+    fun part2() {
+        parseFile()
+
+        val total = leftPoints.sumOf { left ->
+            left * rightPoints.count { it == left }
+        }
+
+        println("Part 2: $total")
+    }
+}
